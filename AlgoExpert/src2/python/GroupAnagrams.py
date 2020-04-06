8bf7fb774b8521594df33dca59e65c6118b215bd
FORNALI Damien <itsdamoy@gmail.com>
2020-04-06 21:13:58 +0200

    Group anagrams other version using defaultdict

--- AlgoExpert/src2/python/GroupAnagrams.py
+++ AlgoExpert/src2/python/GroupAnagrams.py
@@ -10,5 +10,13 @@ def groupAnagrams(words):
 
     return list(res.values())
 
+from collections import defaultdict
+
+def groupAnagrams_v2(words):
+    res = defaultdict(list)
+    for word in words:
+        res['.'.join(sorted(word))].append(word)
+    return [v for v in res.values()]
 
 print(groupAnagrams(["yo", "act", "flop", "tac", "cat", "oy", "olfp"]))
+print(groupAnagrams_v2(["yo", "act", "flop", "tac", "cat", "oy", "olfp"]))
