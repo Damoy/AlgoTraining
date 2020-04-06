49cdca4766ca00c9cbf120ed5e2919b2f857d34b
FORNALI Damien <itsdamoy@gmail.com>
2020-04-06 21:23:55 +0200

    Small update max profit

--- LeetCode/MaxProfit.py
+++ LeetCode/MaxProfit.py
@@ -1,17 +1,9 @@
 def maxProfit(prices):
-    if not len(prices) or len(prices) == 1:
-        return 0
-
-    n = len(prices)
-    if n == 2:
-        return max(0, prices[1] - prices[0])
-
     maxProfitVal = 0
     prevProfit = 0
     
-    for i in range(n - 1):
-        currentPrice = prevProfit + max(0, prices[i + 1] - prices[i])
-        prevProfit = currentPrice
+    for i in range(len(prices) - 1):
+        prevProfit += max(0, prices[i + 1] - prices[i])
         maxProfitVal = max(maxProfitVal, prevProfit)
 
     return maxProfitVal
@@ -19,3 +11,6 @@ def maxProfit(prices):
 print(maxProfit([7,1,5,3,6,4]) == 7)
 print(maxProfit([1,2,3,4,5]) == 4)
 print(maxProfit([7,6,4,3,1]) == 0)
+print(maxProfit([]) == 0)
+print(maxProfit([1,5]) == 4)
+print(maxProfit([5,1]) == 0)
